121eb7dbea0fcd4ec727d923f094511133b32e92
hugo <hugo.herrador@gmail.com>
2023-10-04 21:10:30 +0200

    create and set basic setup for main entity

---
+++ src/files/dto/create-file.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsNumber, IsDateString, IsDate } from 'class-validator';
+export class CreateFileDto {
+  @IsString()
+  readonly file_name: string;
+
+  @IsString()
+  readonly file_type: string;
+
+  @IsNumber()
+  readonly size: number;
+
+  @IsString()
+  readonly uuid?: string;
+
+  @IsString()
+  readonly system_name?: string;
+
+  @IsDate()
+  readonly data: string;
+}
---
+++ src/files/entities/file.entity.ts
@@ -0,0 +1,9 @@
+export class File {
+  id: number;
+  file_name: string;
+  file_type: string;
+  size: number;
+  uuid: string;
+  system_name: string;
+  data: any;
+}
